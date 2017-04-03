75e753e406e3ed5ff96bf6d767e259d3fdcc9aaf
maksym.stepanenko <stepanenkomaksi@gmail.com>
2017-04-03 15:56:24 +0300

    convert binary string to double fraction

--- src/main/java/com/max/algs/AlgorithmsMain.java
+++ src/main/java/com/max/algs/AlgorithmsMain.java
@@ -1,27 +1,9 @@
 package com.max.algs;
 
 
-import com.max.algs.util.MathUtils;
-
 public final class AlgorithmsMain {
 
     private AlgorithmsMain() throws Exception {
-
-        System.out.printf("Huffman codes: %.2f %n", (0.6 * 1 + 0.3 * 2 + 0.05 * 3 + (2 * 0.025 * 4)));
-
-        System.out.printf("Fixed length: %.2f %n", (0.6 * 4 + 0.3 * 4 + 0.05 * 4 + (2 * 0.025 * 4)));
-
-        double[] probabilities = {0.6, 0.3, 0.05, 0.025, 0.025};
-
-        double res = 0.0;
-
-        for (double p : probabilities) {
-            res += p * MathUtils.log2(p);
-        }
-
-        res = -res;
-
-        System.out.printf("entropy: %.2f %n", res);
 
 
         System.out.printf("Main done: java-%s %n", System.getProperty("java.version"));
---
+++ src/main/java/com/max/algs/geeksforgeeks/string/ConvertBinaryFractionToDecimal.java
@@ -0,0 +1,104 @@
+package com.max.algs.geeksforgeeks.string;
+
+import java.util.regex.Pattern;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+final class ConvertBinaryFractionToDecimal {
+
+    private static final Pattern DECIMAL_FRACTION_REGEXP = Pattern.compile("^-?[10]+(\\.[10]+)?$");
+
+    private static void checkCorrectFormat(String str) {
+        if (!DECIMAL_FRACTION_REGEXP.matcher(str).matches()) {
+            throw new IllegalArgumentException("Incorrect string format detected: '" + str + "'");
+        }
+    }
+
+    /**
+     * Convert Binary fraction to Decimal.
+     * <p>
+     * Given an string of binary number n. Convert binary fractional n into it’s decimal equivalent.
+     * <p>
+     * Input: n = 110.101
+     * Output: 6.625
+     * <p>
+     * Input: n = 101.1101
+     * Output: 5.8125
+     * <p>
+     * time: O(N)
+     * space: O(N)
+     *
+     * @see <a href="http://www.geeksforgeeks.org/convert-binary-fraction-decimal/">Convert Binary fraction to Decimal.</a>
+     */
+    public static double convert(String str) {
+        checkNotNull(str);
+        checkArgument(str.length() > 0);
+        checkCorrectFormat(str);
+
+        int sign = 1;
+        int from = 0;
+
+        if (str.charAt(0) == '-') {
+            sign = -1;
+            ++from;
+        }
+
+        int comma = str.indexOf('.');
+
+        double res = realPart(str, from, (comma == -1 ? str.length() - 1 : comma - 1));
+
+        if (comma != -1) {
+            res += decimalPart(str, comma + 1, str.length() - 1);
+        }
+
+        return sign * res;
+    }
+
+    private static double realPart(String str, int from, int to) {
+        double res = 0.0;
+
+        for (int i = from; i <= to; ++i) {
+            res = res * 2.0 + (str.charAt(i) == '0' ? 0.0 : 1.0);
+        }
+
+        return res;
+    }
+
+    private static double decimalPart(String str, int from, int to) {
+
+        double res = 0.0;
+        double val = 1.0 / 2.0;
+
+        for (int i = from; i <= to; ++i) {
+            if (str.charAt(i) == '1') {
+                res += val;
+            }
+
+            val /= 2.0;
+        }
+
+        return res;
+    }
+
+    private ConvertBinaryFractionToDecimal() throws Exception {
+
+        String str = "-101.10011";
+
+        double res = convert(str);
+
+        System.out.printf("res = %.5f %n", res);
+
+        System.out.printf("Main done: java-%s %n", System.getProperty("java.version"));
+    }
+
+    public static void main(String[] args) {
+        try {
+            new ConvertBinaryFractionToDecimal();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
