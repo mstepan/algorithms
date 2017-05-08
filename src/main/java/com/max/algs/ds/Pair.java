965419533767ab9dd2786aa44d65be8b04cbd79b
Maksym Stepanenko <maksym.stepanenko@opower.com>
2017-05-08 14:47:33 +0300

    12.7. Find the min and max simultaneously.

--- src/main/java/com/max/algs/ds/Pair.java
+++ src/main/java/com/max/algs/ds/Pair.java
@@ -6,85 +6,90 @@ import java.util.concurrent.atomic.LongAdder;
 
 /**
  * Immutable pair class.
- * 
- * @author Maksym Stepanenko.
  *
  * @param <T>
+ * @author Maksym Stepanenko.
  */
 public class Pair<K, V> implements Serializable {
 
-	public static final Comparator<Pair<Integer, LongAdder>> KEY_COMPARATOR = new Comparator<Pair<Integer, LongAdder>>(){
+    public static final Comparator<Pair<Integer, LongAdder>> KEY_COMPARATOR = new Comparator<Pair<Integer, LongAdder>>() {
 
-		@Override
-		public int compare(Pair<Integer, LongAdder> o1, Pair<Integer, LongAdder> o2) {
-			return Integer.compare(o1.first, o2.first);
-		}
-		
-	};
+        @Override
+        public int compare(Pair<Integer, LongAdder> o1, Pair<Integer, LongAdder> o2) {
+            return Integer.compare(o1.first, o2.first);
+        }
 
-	private static final long serialVersionUID = -6384976076282848373L;
+    };
 
-	protected final K first;
-	protected final V second;
+    private static final long serialVersionUID = -6384976076282848373L;
 
-	private int hash;
+    protected final K first;
+    protected final V second;
 
-	public Pair(K first, V second) {
-		super();
-		this.first = first;
-		this.second = second;
-	}
+    private int hash;
 
-	public K getFirst() {
-		return first;
-	}
+    public Pair(K first, V second) {
+        super();
+        this.first = first;
+        this.second = second;
+    }
 
-	public V getSecond() {
-		return second;
-	}
+    public static <T> Pair<T, T> of(T val1, T val2) {
+        return new Pair<>(val1, val2);
+    }
 
-	@Override
-	public int hashCode() {
+    public static <T> Pair<T, T> empty() {
+        return new Pair<>(null, null);
+    }
 
-		if (hash == 0) {
-			int res = 1;
-			res = 31 * res + ((first == null) ? 0 : first.hashCode());
-			res = 31 * res + ((second == null) ? 0 : second.hashCode());
-			hash = res;
-		}
+    public K getFirst() {
+        return first;
+    }
 
-		return hash;
-	}
+    public V getSecond() {
+        return second;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pair<?, ?> other = (Pair<?, ?>) obj;
-		if (first == null) {
-			if (other.first != null)
-				return false;
-		}
-		else
-			if (!first.equals(other.first))
-				return false;
-		if (second == null) {
-			if (other.second != null)
-				return false;
-		}
-		else
-			if (!second.equals(other.second))
-				return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
 
-	@Override
-	public String toString() {
-		return "(" + first + ", " + second + ")";
-	}
+        if (hash == 0) {
+            int res = 1;
+            res = 31 * res + ((first == null) ? 0 : first.hashCode());
+            res = 31 * res + ((second == null) ? 0 : second.hashCode());
+            hash = res;
+        }
+
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pair<?, ?> other = (Pair<?, ?>) obj;
+        if (first == null) {
+            if (other.first != null)
+                return false;
+        }
+        else if (!first.equals(other.first))
+            return false;
+        if (second == null) {
+            if (other.second != null)
+                return false;
+        }
+        else if (!second.equals(other.second))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
 
 }
---
+++ src/main/java/com/max/algs/epi/searching/FindMinMaxSimultaneously.java
@@ -0,0 +1,68 @@
+package com.max.algs.epi.searching;
+
+import com.max.algs.ds.Pair;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * 12.7. Find the min and max simultaneously.
+ */
+public class FindMinMaxSimultaneously {
+
+    /**
+     * time: O(N)
+     * space: O(1)
+     * <p>
+     * comparisons count: 3 * (N/2)
+     */
+    public static Pair<Integer, Integer> finMinMax(int[] arr) {
+        checkNotNull(arr);
+
+        if (arr.length == 0) {
+            return Pair.empty();
+        }
+
+        int minSoFar = Integer.MAX_VALUE;
+        int maxSoFar = Integer.MIN_VALUE;
+
+        for (int i = 1; i < arr.length; i += 2) {
+            int minCur = arr[i - 1];
+            int maxCur = arr[i];
+
+            if (minCur > maxCur) {
+                minCur = arr[i];
+                maxCur = arr[i - 1];
+            }
+
+            minSoFar = Math.min(minSoFar, minCur);
+            maxSoFar = Math.max(maxSoFar, maxCur);
+        }
+
+        // odd case
+        if ((arr.length & 1) != 0) {
+            minSoFar = Math.min(minSoFar, arr[arr.length - 1]);
+            maxSoFar = Math.max(maxSoFar, arr[arr.length - 1]);
+        }
+
+        return Pair.of(minSoFar, maxSoFar);
+    }
+
+    private FindMinMaxSimultaneously() throws Exception {
+
+        int[] arr = {5, 3, 3, 2, 12, 12, 8, 12, -6, -5, -33};
+
+        System.out.println(finMinMax(arr));
+
+        System.out.printf("'FindMinMaxSimultaneously' completed. java-%s %n", System.getProperty("java.version"));
+    }
+
+    public static void main(String[] args) {
+        try {
+            new FindMinMaxSimultaneously();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
