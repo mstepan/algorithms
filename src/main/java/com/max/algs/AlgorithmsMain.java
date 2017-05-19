acc79c6b73aeddba6b01fab33bdec8de4519547e
Maksym Stepanenko <maksym.stepanenko@opower.com>
2017-05-19 15:57:19 +0300

    EPI tasks added. Find missed element and duplicate from unsorted array.

--- src/main/java/com/max/algs/AlgorithmsMain.java
+++ src/main/java/com/max/algs/AlgorithmsMain.java
@@ -1,23 +1,11 @@
 package com.max.algs;
 
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static java.util.Arrays.stream;
-
 public final class AlgorithmsMain {
+
 
     private AlgorithmsMain() throws Exception {
 
-        List<Integer> data = stream(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}).
-                map(val -> val * val).
-                filter(val -> (val & 1) == 0).
-                sorted().
-                boxed().
-                collect(Collectors.toList());
-
-        System.out.println(data);
 
         System.out.printf("Main done: java-%s %n", System.getProperty("java.version"));
     }
