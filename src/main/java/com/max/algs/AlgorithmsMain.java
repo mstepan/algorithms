f89e78e07d6c413e03383d178bab98bc37b83a41
Maksym Stepanenko <maksym.stepanenko@opower.com>
2017-06-13 12:31:55 +0300

    Count-Min sketch probabalistic DS added.

--- src/main/java/com/max/algs/AlgorithmsMain.java
+++ src/main/java/com/max/algs/AlgorithmsMain.java
@@ -1,59 +1,76 @@
 package com.max.algs;
 
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import com.max.algs.ds.probabalistic.CountMinSketch;
+import com.max.algs.util.MathUtils;
 
 public final class AlgorithmsMain {
 
+    private static final double LN_2 = MathUtils.ln(2.0);
+    private static final double LN_2_SQUARED = Math.pow(LN_2, 2.0);
 
     /**
-     * Use backtracking.
-     *
-     * N - str.length()
-     *
-     * space: O(N)
+     * See: https://stackoverflow.com/questions/658439/how-many-hash-functions-does-my-bloom-filter-need
+     * <p>
+     * Given:
+     * <p>
+     * n: how many items you expect to have in your filter (e.g. 216,553)
+     * p: your acceptable false positive rate {0..1} (e.g. 0.01 → 1%) we want to calculate:
+     * <p>
+     * m: the number of bits needed in the bloom filter
+     * k: the number of hash functions we should apply
+     * The formulas:
+     * <p>
+     * m = -n*ln(p) / (ln(2)^2) the number of bits
+     * k = m/n * ln(2) the number of hash functions
+     * <p>
+     * In our case:
+     * <p>
+     * m = -216553*ln(0.01) / (ln(2)^2) = 997263 / 0.48045 = 2,075,686 bits (253 kB)
+     * k = m/n * ln(2) = 2075686/216553 * 0.693147 = 6.46 hash functions (7 hash functions)
      */
-    public static void printAllValues(String str) {
-        checkNotNull(str);
+    private static final void bloomFilterParameters() {
 
-        System.out.println(str);
+        // expected number of elements
+        int n = 1000;
 
-        char[] arr = str.toCharArray();
+        // accepted false positive error rate, 3%
+        double p = 0.01;
 
-        for (int i = 0; i < arr.length; ++i) {
+        // bits count for bloom filter
+        int m = (int) Math.round(-n * MathUtils.ln(p) / LN_2_SQUARED);
 
-            char prev = arr[i];
-            arr[i] = '1';
+        // hash functions count for bloom filter
+        int k = (int) Math.round(((double) m / n) * LN_2);
 
-            printRec(new String(arr), i);
+        System.out.printf("m = %d, k = %d %n", m, k);
 
-            arr[i] = prev;
-        }
+        // p = e^(-(m / n) * (ln(2)^2))
+        double pActual = Math.pow(Math.E, (-(m / n) * LN_2_SQUARED));
+
+        System.out.printf("pActual = %3f %n", pActual);
     }
 
-    private static void printRec(String str, int index) {
-        System.out.println(str);
-
-        if (index == str.length() - 1) {
-            return;
-        }
-
-        int count = str.charAt(index) - '0';
-
-        if (count == 9) {
-            return;
-        }
-
-        ++count;
-
-        String newStr = str.substring(0, index) + String.valueOf(count) + str.substring(index + 2, str.length());
-
-        printRec(newStr, index);
-    }
 
     private AlgorithmsMain() throws Exception {
+//        bloomFilterParameters();
 
-        printAllValues("ABCD");
+        CountMinSketch data = new CountMinSketch(0.01, 0.90);
+
+        data.add("192.169.1.1");
+        data.add("192.169.1.1");
+        data.add("192.169.1.1");
+
+        data.add("192.169.1.2");
+        data.add("192.169.1.2");
+
+        data.add("192.169.1.3");
+
+        System.out.println(data.getCount("192.169.1.1"));
+        System.out.println(data.getCount("192.169.1.2"));
+        System.out.println(data.getCount("192.169.1.3"));
+
+        System.out.println(data.getCount("192.169.1.4"));
 
         System.out.printf("Main done: java-%s %n", System.getProperty("java.version"));
     }
---
+++ src/main/java/com/max/algs/ds/probabalistic/CountMinSketch.java
@@ -0,0 +1,104 @@
+package com.max.algs.ds.probabalistic;
+
+import com.max.algs.hashing.universal.UniversalHashFunction;
+import com.max.algs.util.MathUtils;
+
+/**
+ * Count-min sketch frequency calculation data structure.
+ */
+public class CountMinSketch {
+
+    // ε (Epsilon) is 'how much error is added to our counts with each item we add to the sketch', accuracy (1%)
+    private static final double DEFAULT_EPSILON = 0.01;
+
+    // δ (Delta) is 'with what probability do we want to allow the count estimate to be outside of
+    // our DEFAULT_EPSILON error rate', 1-δ = 0.99 (99% that we are within our accuracy)
+    private static final double DEFAULT_DELTA = 0.01;
+
+    private final int depth;
+    private final int width;
+
+    private final int[][] counters;
+    private final UniversalHashFunction<String>[] hashFunctions;
+
+    public CountMinSketch(double errorRate, double accuracy) {
+        this(errorRate, 1.0 - accuracy, 0);
+    }
+
+    public CountMinSketch() {
+        this(DEFAULT_EPSILON, DEFAULT_DELTA, 0);
+    }
+
+    @SuppressWarnings("unchecked")
+    private CountMinSketch(double epsilon, double delta, int dummy) {
+
+        width = calculateWidth(epsilon);
+        depth = calculateDepth(delta);
+
+        System.out.println("width = " + width);
+        System.out.println("depth = " + depth);
+
+        counters = new int[depth][width];
+
+        hashFunctions = new UniversalHashFunction[counters.length];
+
+        for (int i = 0; i < hashFunctions.length; ++i) {
+            hashFunctions[i] = UniversalHashFunction.generate();
+        }
+    }
+
+    private int calculateDepth(double deltaValue) {
+        return (int) Math.ceil(MathUtils.ln(1.0 / deltaValue));
+    }
+
+    private int calculateWidth(double epsilonValue) {
+        return (int) Math.ceil(Math.E / epsilonValue);
+    }
+
+    public void add(String value) {
+        applyLogicToCounters(value, new IncCounters());
+    }
+
+    public int getCount(String value) {
+
+        FindMinCounter findMinCounter = new FindMinCounter();
+
+        applyLogicToCounters(value, findMinCounter);
+
+        return findMinCounter.cnt == Integer.MAX_VALUE ? 0 : findMinCounter.cnt;
+    }
+
+    private void applyLogicToCounters(String value, CountersVisitor handler) {
+
+        for (int i = 0; i < hashFunctions.length; ++i) {
+            UniversalHashFunction<String> func = hashFunctions[i];
+
+            int hashValue = (func.hash(value) & 0x7F_FF_FF_FF) % width;
+
+            handler.handle(i, hashValue);
+        }
+    }
+
+    @FunctionalInterface
+    private interface CountersVisitor {
+        void handle(int row, int col);
+    }
+
+    private class IncCounters implements CountersVisitor {
+        @Override
+        public void handle(int row, int col) {
+            ++CountMinSketch.this.counters[row][col];
+        }
+    }
+
+    private class FindMinCounter implements CountersVisitor {
+
+        private int cnt = Integer.MAX_VALUE;
+
+        @Override
+        public void handle(int row, int col) {
+            cnt = Math.min(CountMinSketch.this.counters[row][col], cnt);
+        }
+    }
+
+}
