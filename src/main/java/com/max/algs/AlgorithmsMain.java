617502d73dc888eb0dfc231754823174b19e9992
Maksym Stepanenko <maksym.stepanenko@opower.com>
2017-11-26 13:07:34 +0200

    equals tester added

--- src/main/java/com/max/algs/AlgorithmsMain.java
+++ src/main/java/com/max/algs/AlgorithmsMain.java
@@ -8,11 +8,24 @@ import java.lang.invoke.MethodHandles;
 
 public final class AlgorithmsMain {
 
-
     private static final Logger LOG = Logger.getLogger(MethodHandles.lookup().lookupClass());
 
-
     private AlgorithmsMain() {
+
+//        MyPoint a = new MyPoint(0, 0, 1);
+//        MyPoint b = new MyPoint(1, 1, 5);
+//        MyPoint c = new MyPoint(2, 2, Integer.MIN_VALUE + 2);
+//
+//        LOG.info("a.compareTo(b): " + a.compareTo(b));
+//        LOG.info("b.compareTo(c): " + b.compareTo(c));
+//        LOG.info("a.compareTo(c): " + a.compareTo(c));
+
+        LOG.info(Integer.MIN_VALUE);
+
+        int res = 10 - Integer.MIN_VALUE;
+
+        LOG.info(res);
+
         LOG.info("AlgorithmsMain done...");
     }
 
@@ -25,4 +38,6 @@ public final class AlgorithmsMain {
             LOG.error(ex.getMessage(), ex);
         }
     }
+
+
 }
