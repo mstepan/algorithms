6081883ec178861cbcbb919e28339edbf17afe53
Maksym Stepanenko <maksym.stepanenko@opower.com>
2017-12-31 14:57:47 +0200

    Shortest path in graph benchmark added.

--- src/main/java/benchmark/graph/ShortestPathInGraphMain.java
+++ src/main/java/benchmark/graph/ShortestPathInGraphMain.java
@@ -4,51 +4,16 @@ package benchmark.graph;
 import org.apache.log4j.Logger;
 
 import java.lang.invoke.MethodHandles;
-import java.util.Random;
 
 
 public final class ShortestPathInGraphMain {
 
     private static final Logger LOG = Logger.getLogger(MethodHandles.lookup().lookupClass());
 
-    private static final Random RAND = new Random();
-
-    private static void randomPermutation(String[] arr) {
-        for (int i = 0; i < arr.length - 1; ++i) {
-            swap(arr, i, i + RAND.nextInt(arr.length - i));
-        }
-    }
-
-    private static void swap(String[] arr, int from, int to) {
-        assert arr != null;
-        assert (from >= 0 && from < arr.length) && (to >= 0 && to < arr.length);
-
-        String temp = arr[from];
-        arr[from] = arr[to];
-        arr[to] = temp;
-    }
-
-    private static String[] generateVertexesLabels(int length) {
-        String[] labels = new String[length];
-
-        final char firstCh = 'A';
-        final char lastCh = 'Z';
-
-        char baseLabel = firstCh;
-
-        for (int i = 0; i < labels.length; ++i) {
-            labels[i] = String.valueOf(baseLabel) + "-" + i;
-            baseLabel = (baseLabel == lastCh) ? firstCh : (char) (baseLabel + 1);
-        }
-
-        randomPermutation(labels);
-        return labels;
-    }
-
     private ShortestPathInGraphMain() {
 
         for (int it = 0; it < 100; ++it) {
-            String[] labels = generateVertexesLabels(10_000);
+            String[] labels = ShortestPathInGraphBenchmark.generateVertexesLabels(10_000);
 
             DirectAcyclicGraph graph = DagGenerator.generate(labels);
 
@@ -62,7 +27,7 @@ public final class ShortestPathInGraphMain {
             int simpleShortestPath = graph.shortestPath(src, dest);
             int dijkstraShortestPath = graph.shortestPathDijkstra(src, dest);
 
-            if( simpleShortestPath != dijkstraShortestPath ){
+            if (simpleShortestPath != dijkstraShortestPath) {
                 throw new IllegalStateException("Paths aren't equals: simple = " + simpleShortestPath +
                         ", dijkstra = " + dijkstraShortestPath);
             }
