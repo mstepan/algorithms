b698c39583ef3a0ec84e44f8558752526903afce
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-12-17 13:20:46 +0200

    Needleman-Wunsch algorithms (Levenshtein distance) implemented. Classic version and d-changes version.

--- src/main/java/com/max/algs/string/EditDistance.java
+++ src/main/java/com/max/algs/string/EditDistance.java
@@ -1,15 +1,108 @@
 package com.max.algs.string;
 
 
+import java.util.Arrays;
+
 import static com.google.common.base.Preconditions.checkArgument;
 
-public final class EditDistance {
+/**
+ * Global alignment Needleman-Wunsch algorithm (a.k.a edit distance).
+ * See wiki: https://en.wikipedia.org/wiki/Needleman%E2%80%93Wunsch_algorithm
+ */
+final class EditDistance {
 
     private EditDistance() {
         throw new IllegalStateException("Can't initialize utility only class");
     }
 
-    public static int editDistance(String str, String other) {
+    /**
+     * Check if String 'str' can be converted to 'other' string using at most 'd'
+     * transformations (insertion, deletion, replacement).
+     * <p>
+     * <p>
+     * N - str.length()
+     * M - other.length()
+     * <p>
+     * time: O(d * min(N, M))
+     * space: O(d)
+     */
+    static boolean canEditWithDistance(String str, String other, int d) {
+        checkArgument(str != null, "null 'str' passed");
+        checkArgument(other != null, "null 'other' passed");
+        checkArgument(d >= 0, "negative distance passed");
+
+        if (d == 0) {
+            return str.equals(other);
+        }
+
+        if (Math.abs(str.length() - other.length()) > d) {
+            return false;
+        }
+
+        if (Math.max(str.length(), other.length()) <= d) {
+            return true;
+        }
+
+        if (str.length() >= other.length()) {
+            return canEditWithDistanceInternal(str, other, d);
+        }
+
+        return canEditWithDistanceInternal(other, str, d);
+    }
+
+
+    private static boolean canEditWithDistanceInternal(String str, String other, int d) {
+
+        final int rows = other.length() + 1;
+        final int cols = (2 * d) + 3;
+        final int median = d + 1;
+
+        int[] prev = new int[cols];
+
+        Arrays.fill(prev, Integer.MAX_VALUE);
+        for (int i = median, index = 0; i < prev.length - 1; ++i, ++index) {
+            prev[i] = index;
+        }
+
+        for (int rowIndex = 1, colIndex = 1; rowIndex < rows; ++rowIndex, ++colIndex) {
+            int[] cur = new int[cols];
+            Arrays.fill(cur, Integer.MAX_VALUE);
+
+            char otherCh = other.charAt(rowIndex - 1);
+
+            int colOffset = colIndex - median;
+
+            for (int i = 1; i < cur.length - 1; ++i) {
+
+                int realCol = i + colOffset;
+
+                if (realCol == 0) {
+                    cur[i] = rowIndex <= d ? rowIndex : Integer.MAX_VALUE;
+                }
+                else if (realCol > 0 && realCol <= str.length()) {
+
+                    char strCh = str.charAt(realCol - 1);
+
+                    if (strCh == otherCh) {
+                        cur[i] = prev[i];
+                    }
+                    else {
+                        cur[i] = 1 + min(prev[i], prev[i + 1], cur[i - 1]);
+                    }
+                }
+            }
+
+            prev = cur;
+        }
+
+        if (prev[median] == Integer.MAX_VALUE) {
+            return false;
+        }
+
+        return prev[median] <= d;
+    }
+
+    static int editDistance(String str, String other) {
 
         checkArgument(str != null, "null 'str' passed");
         checkArgument(other != null, "null 'other' passed");
--- src/test/java/com/max/algs/string/EditDistanceTest.java
+++ src/test/java/com/max/algs/string/EditDistanceTest.java
@@ -2,15 +2,42 @@ package com.max.algs.string;
 
 import org.junit.Test;
 
+import static com.max.algs.string.EditDistance.canEditWithDistance;
 import static com.max.algs.string.EditDistance.editDistance;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class EditDistanceTest {
+
+    @Test
+    public void canBeEditedUsingSpecifiedDistance() {
+
+        assertFalse(canEditWithDistance("AGCATGC", "ACAATCC", 0));
+        assertFalse(canEditWithDistance("AGCATGC", "ACAATCC", 1));
+        assertFalse(canEditWithDistance("AGCATGC", "ACAATCC", 2));
+
+        assertTrue(canEditWithDistance("AGCATGC", "ACAATCC", 3));
+        assertTrue(canEditWithDistance("AGCATGC", "ACAATCC", 4));
+        assertTrue(canEditWithDistance("AGCATGC", "ACAATCC", 10));
+
+        assertTrue(canEditWithDistance("A8CATGC", "ACAAT2C", 3));
+
+        assertTrue(canEditWithDistance("AAACCC", "AAA", 3));
+        assertFalse(canEditWithDistance("AAACCCC", "AAA", 3));
+    }
 
     @Test
     public void editDistanceNormalFlow() {
 
         assertEquals(3, editDistance("AGCATGC", "ACAATCC"));
+        assertEquals(3, editDistance("AGCATGC", "ACAATCC"));
+        assertEquals(3, editDistance("AGCATGC", "ACAATCC"));
+
+        assertEquals(3, editDistance("AGCATGC", "ACAATCC"));
+        assertEquals(3, editDistance("AGCATGC", "ACAATCC"));
+        assertEquals(3, editDistance("AGCATGC", "ACAATCC"));
+
         assertEquals(3, editDistance("A8CATGC", "ACAAT2C"));
 
         assertEquals(0, editDistance("AAAA", "AAAA"));
@@ -25,11 +52,13 @@ public class EditDistanceTest {
         assertEquals(7, editDistance("AGCATGC", ""));
     }
 
+    @SuppressWarnings("ConstantConditions")
     @Test(expected = IllegalArgumentException.class)
     public void editDistanceNullFirstParameterFails() {
         editDistance(null, "ACAATCC");
     }
 
+    @SuppressWarnings("ConstantConditions")
     @Test(expected = IllegalArgumentException.class)
     public void editDistanceNullSecondParameterFails() {
         editDistance("ACAATCC", null);
