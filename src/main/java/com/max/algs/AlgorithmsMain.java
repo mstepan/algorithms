34b5a0ed0c65caa1fb80f8cf71b14f4ce929c7a0
Maksym Stepanenko <maksym.stepanenko@opower.com>
2019-02-15 13:46:52 +0200

    Java object layout as maven dependency added.

--- src/main/java/com/max/algs/AlgorithmsMain.java
+++ src/main/java/com/max/algs/AlgorithmsMain.java
@@ -1,15 +1,23 @@
 package com.max.algs;
 
 import org.apache.log4j.Logger;
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.vm.VM;
+
+import java.util.HashMap;
 
 public final class AlgorithmsMain {
 
     private static final Logger LOG = Logger.getLogger(AlgorithmsMain.class);
 
-    private AlgorithmsMain() {
+    private AlgorithmsMain() throws Exception {
+
+        System.out.println(VM.current().details());
+        System.out.println(ClassLayout.parseClass(HashMap.class).toPrintable());
 
         LOG.info("Main done... java-" + System.getProperty("java.version"));
     }
+
 
     public static void main(String[] args) {
         try {
