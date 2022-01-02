40cd6e23003d03ec27974bc923337d599bec3f8e
Maksym Stepanenko <maksym.stepanenko@opower.com>
2022-01-02 13:28:20 +0200

    Cleanup.

--- target/classes/db/migration/V1__create_users_table.sql
+++
@@ -1,4 +0,0 @@
-create table MY_USER (
-    ID int not null primary key,
-    login varchar(100) not null unique
-);
--- target/classes/db/migration/V2__insert_users.sql
+++
@@ -1,2 +0,0 @@
-insert into MY_USER(ID, login) values (1, 'max');
-insert into MY_USER(ID, login) values (2, 'olesia');
